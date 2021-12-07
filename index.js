3daf58d0851bcbb761cb647f22a425bdff1426c2
madhanse29 <srkmadhan@gmail.com>
2021-12-07 11:16:26 +0530

    comments

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
+//import express
 const express = require("express");
-
+//import ffmpeg
 const ffmpeg = require("fluent-ffmpeg");
-
+//import bodyParser
 const bodyParser = require("body-parser");
 
 const fs = require("fs");
-
+//for file upload
 const fileUpload = require("express-fileupload");
 
 const app = express();
-
+//port no
 const PORT = process.env.PORT || 5000
 
 // parse application/x-www-form-urlencoded
@@ -26,39 +27,38 @@ app.use(
     tempFileDir: "/tmp/",
   })
 );
-
+//set poth from local foe ffmpeg conversion
 ffmpeg.setFfmpegPath("C:/PATH_PROGRAMS/ffmpeg.exe");
 
 ffmpeg.setFfprobePath("C:/PATH_PROGRAMS");
 
 console.log(ffmpeg);
-
+//for getting html file
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
+//after the convert button click
 app.post("/convert", (req, res) => {
-  //res.contentType(`video/${to}`);
-  //res.attachment(`output.${to}`
 
   let to = req.body.to;
   let file = req.files.file;
   let fileName = `output.${to}`;
   console.log(to);
   console.log(file);
-
+//mv function for upload
   file.mv("tmp/" + file.name, function (err) {
     if (err) return res.sendStatus(500).send(err);
     console.log("File Uploaded successfully");
   });
-
+//create file in temp folder and download after conversion
   ffmpeg("tmp/" + file.name)
     .withOutputFormat(to)
     .on("end", function (stdout, stderr) {
       console.log("Finished");
       res.download(__dirname + fileName, function (err) {
         if (err) throw err;
-
+//after conversion file is deleted
         fs.unlink(__dirname + fileName, function (err) {
           if (err) throw err;
           console.log("File deleted");
